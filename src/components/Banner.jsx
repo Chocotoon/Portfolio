7b55b7e5b3f9198c7160e18de5239223b6ddc308
nicok <khamsophan@yahoo.fr>
2024-06-24 11:35:36 +0200

    ajout modale

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,26 @@
 import BannerImg from "../assets/photo-portfolio.png"
 import Button from "./Button"
+import { useState } from 'react'
+import Modal from 'react-bootstrap/Modal'
 import CV from "../assets/CV.png"
 function Banner({ hContent, children }) {
+
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+
     return (
         <div className="row gx-5 pt-5">
             <div className="col-md-8 col pt-5 text-md-start text-center">
                 <h1 className="fw-bold">{hContent}</h1><br />
                 <p>{children}</p>
-                <Button path={CV} target="_blank">Voir mon CV</Button>
+                <button className="btn bg-primary bg-gradient text-light fw-bold mb-2" onClick={handleShow}>Voir mon CV</button>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton >
+                    </Modal.Header>
+                    <Modal.Body ><img src={CV} alt="" style={{ width: '100%' }}></img></Modal.Body>
+                </Modal>
             </div>
             <div className="col-md-4 col d-flex justify-content-center">
                 <img className="rounded-2" src={BannerImg} alt='Nicolas Khamsopha' />
